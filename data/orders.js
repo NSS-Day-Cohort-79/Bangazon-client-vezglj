abc272122bd12ed67b97d7fecb47450019555e0b
Maggie Flatt <maggieflatt19@gmail.com>
2026-04-17 12:01:56 -0500

    fixed cart error that prevented cart from showing.

--- data/orders.js
+++ data/orders.js
@@ -1,7 +1,7 @@
 import { fetchWithResponse } from "./fetcher";
 
 export function getCart() {
-  return fetchWithResponse("cårt", {
+  return fetchWithResponse("cart", {
     headers: {
       Authorization: `Token ${localStorage.getItem("token")}`,
     },
