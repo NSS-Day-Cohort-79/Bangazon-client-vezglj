ce880051aa2e41d45c15e874991f3f2bea47dcfa
Jordan Haddock <haddockjordan40@gmail.com>
2026-04-22 14:33:45 -0500

    initial commit

--- data/fetcher.js
+++ data/fetcher.js
@@ -1,34 +1,36 @@
-const API_URL = 'http://localhost:8000'
+const API_URL = "http://localhost:8000";
 
 const checkError = (res) => {
   if (!res.ok) {
     throw Error(res.status);
   }
-  return res
-}
+  return res;
+};
 
 const checkErrorJson = (res) => {
-  if (res.status !== 200) {
+  if (!res.ok) {
     throw Error(res.status);
   } else {
-    return res.json()
+    return res.json();
   }
-}
-
+};
 
 const catchError = (err) => {
-  if (err.message === '401') {
-    window.location.href = "/login"
+  console.log("fetch error", err.message);
+
+  if (err.message === "401") {
+    window.location.href = "/login";
   }
-  if (err.message === '404') {
+  if (err.message === "404") {
     throw Error(err.message);
   }
-}
+  throw Error("An error occurred while fetching data");
+};
 
-export const fetchWithResponse = (resource, options) => fetch(`${API_URL}/${resource}`, options)
-  .then(checkErrorJson)
-  .catch(catchError)
+export const fetchWithResponse = (resource, options) =>
+  fetch(`${API_URL}/${resource}`, options)
+    .then(checkErrorJson)
+    .catch(catchError);
 
-export const fetchWithoutResponse = (resource, options) => fetch(`${API_URL}/${resource}`, options)
-  .then(checkError)
-  .catch(catchError)
+export const fetchWithoutResponse = (resource, options) =>
+  fetch(`${API_URL}/${resource}`, options).then(checkError).catch(catchError);
--- pages/stores/new.js
+++ pages/stores/new.js
@@ -1,36 +1,47 @@
-import { useRouter } from 'next/router'
-import { useRef } from 'react'
-import Layout from '../../components/layout'
-import Navbar from '../../components/navbar'
-import { addStore } from '../../data/stores'
-import { useAppContext } from '../../context/state'
-import StoreForm from '../../components/store/form'
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import Layout from "../../components/layout";
+import Navbar from "../../components/navbar";
+import { addStore } from "../../data/stores";
+import { useAppContext } from "../../context/state";
+import StoreForm from "../../components/store/form";
 
 export default function NewStore() {
-  const { setProfile, profile } = useAppContext()
+  const { setProfile, profile } = useAppContext();
 
-  const nameEl = useRef()
-  const descriptionEl = useRef()
-  const router = useRouter()
+  const nameEl = useRef();
+  const descriptionEl = useRef();
+  const router = useRouter();
 
   const saveStore = () => {
     addStore({
       name: nameEl.current.value,
-      description: descriptionEl.current.value
+      description: descriptionEl.current.value,
     }).then((res) => {
+      if (!res) return;
       setProfile({
         ...profile,
-        store: res
-      })
-      router.push(`/stores/${res.id}`)
-    })
-  }
+        stores: [...profile.stores, res],
+      });
+
+      router.push(`/stores/${res.id}`);
+    });
+  };
 
   return (
-    <StoreForm nameEl={nameEl} descriptionEl={descriptionEl} saveEvent={saveStore} router={router} title="Create your store">
-      <p>Give your new store a name and description. Then add products on the next page</p>
+    <StoreForm
+      nameEl={nameEl}
+      descriptionEl={descriptionEl}
+      saveEvent={saveStore}
+      router={router}
+      title="Create your store"
+    >
+      <p>
+        Give your new store a name and description. Then add products on the
+        next page
+      </p>
     </StoreForm>
-  )
+  );
 }
 
 NewStore.getLayout = function getLayout(page) {
@@ -39,5 +50,5 @@ NewStore.getLayout = function getLayout(page) {
       <Navbar />
       {page}
     </Layout>
-  )
-}
+  );
+};
